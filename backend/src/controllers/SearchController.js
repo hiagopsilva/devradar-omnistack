4ec4d9231653cfe5a5298b5abf275ae5d0271de8
hiagopsilva <hiagohps360@gmail.com>
2020-06-28 02:19:11 -0300

    backend

---
+++ backend/src/controllers/SearchController.js
@@ -0,0 +1,27 @@
+const Dev = require('../models/Dev');
+const parseStringAsArray = require('../utils/parseStringAsArray');
+
+module.exports = {
+  async index(req, res) {
+    const { latitude, longitude, techs } = req.query;
+
+    const tehcsArray = parseStringAsArray(techs);
+
+    const devs = await Dev.find({
+      techs: {
+        $in: tehcsArray,
+      },
+      location: {
+        $near: {
+          $geometry: {
+            type: 'Point',
+            coordinates: [longitude, latitude],
+          },
+          $maxDistance: 10000,
+        },
+      },
+    });
+    
+    return res.json({ devs });
+  }
+}
---
+++ backend/src/index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const http = require('http');
+
+const routes = require('./routes');
+const { setupWebSocket } = require('./websocket');
+
+const app = express();
+const server = http.Server(app);
+
+setupWebSocket(server);
+
+mongoose.connect('mongodb+srv://omnistack:omnistack@cluster0-7golp.mongodb.net/week10?retryWrites=true&w=majority', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+
+app.use(cors());
+app.use(express.json());
+app.use(routes);
+
+server.listen(process.env.PORT || 3333);
